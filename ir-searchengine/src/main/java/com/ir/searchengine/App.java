b94a52a5b9adc5386b420c4c67647a80a6df0f5a
Shawn <myking123321@gmail.com>
2025-05-06 09:33:33 +0700

    Initialize Maven Project

---
+++ ir-searchengine/src/main/java/com/ir/searchengine/App.java
@@ -0,0 +1,13 @@
+package com.ir.searchengine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
