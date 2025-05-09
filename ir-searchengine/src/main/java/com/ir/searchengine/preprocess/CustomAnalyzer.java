7863b59bbffc019ff6890eaf0017657f3023c2ce
Shawn <myking123321@gmail.com>
2025-05-09 11:00:19 +0700

    add: custom anaylzer with stopwords

--- ir-searchengine/src/main/java/com/ir/searchengine/preprocess/CustomAnalyzer.java
+++ ir-searchengine/src/main/java/com/ir/searchengine/preprocess/CustomAnalyzer.java
@@ -1,18 +1,29 @@
 package com.ir.searchengine.preprocess;
 
-import java.io.FileReader;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.CharArraySet;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 
 public class CustomAnalyzer {
     public static StandardAnalyzer getCustomAnalyzer() throws IOException{
-        try( Reader reader = new InputStreamReader(CustomAnalyzer.class.getClassLoader().getResourceAsStream("stopwords.txt"))){
-        
-            return new StandardAnalyzer(reader);
+
+        InputStream inputStream = CustomAnalyzer.class.getClassLoader().getResourceAsStream("stopwords.txt");
+
+        try (BufferedReader reader = new BufferedReader( new InputStreamReader(inputStream))){
+            List<String> stopwords = reader.lines().map(String::trim)
+                                                .filter(line -> !line.isEmpty())
+                                                .collect(Collectors.toList());
+
+            CharArraySet stopSet = new CharArraySet(stopwords, true);
+            return new StandardAnalyzer(stopSet);
         }
+
+        
     }
 }
