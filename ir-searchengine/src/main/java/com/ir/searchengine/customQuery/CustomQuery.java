728ae83ee22e36abafc8bf205b5aeb1f0f2d0674
Shawn <myking123321@gmail.com>
2025-05-15 08:08:48 +0700

    fix: rename query folder in main to customQuery

---
+++ ir-searchengine/src/main/java/com/ir/searchengine/customQuery/CustomQuery.java
@@ -0,0 +1,45 @@
+package com.ir.searchengine.query;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.ir.searchengine.preprocess.Preprocess;
+
+public class CustomQuery {
+    private String queryText;
+    private List<String> tokens;
+
+    public CustomQuery(String queryText){
+        this.queryText = queryText;
+    }
+
+    public String process(){
+
+        StringBuilder result = new StringBuilder();
+        try {
+            this.tokens = Preprocess.tokenize("field", queryText);
+       
+            for (String token : tokens){
+                result.append(token+" ");
+            }
+        } catch (IOException e) {
+            
+            e.printStackTrace();
+        }
+
+        return result.toString();
+    }
+
+    public String getQueryText() {
+        return queryText;
+    }
+
+    public List<String> getTokens() {
+        return tokens;
+    }
+
+    
+
+    
+
+}
